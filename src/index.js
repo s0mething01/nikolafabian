ce77bfbbaf34843408f3ee0d3ed6b6d08c08099d
Mikołaj <miki70707@interia.pl>
2020-02-11 00:13:35 +0100

    webfontloader added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import WebFont from 'webfontloader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const webFontConfig = {
+    google: {
+      families: ['Montserrat:400,500,700:latin-ext'],
+    }
+};
+
+WebFont.load(webFontConfig);
